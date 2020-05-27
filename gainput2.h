e881db4441f7c9d8987527b1c2af99c2ebc36bac
Johannes Kuhlmann <git@johanneskuhlmann.de>
2020-05-27 21:32:08 +0200

    Update style to fit the popular choice.

--- gainput2.h
+++ gainput2.h
@@ -1,3 +1,9 @@
+/**
+ * gainput2 - C input library for games.
+ *
+ * Copyright (c) 2013-2020 Johannes Kuhlmann.
+ * Licensed under the MIT license. See LICENSE file.
+ */
 
 #ifndef GAINPUT2_H_
 #define GAINPUT2_H_
@@ -10,118 +16,113 @@
 extern "C" {
 #endif
 
-typedef bool GpBool;
-typedef float GpFloat;
-typedef size_t GpSize;
+typedef bool gp_bool;
+typedef float gp_float;
+typedef size_t gp_size;
 
-typedef uint64_t GpContext;
-typedef uint64_t GpDevice;
-typedef uint64_t GpDeviceButton;
+typedef uint64_t gp_context;
+typedef uint64_t gp_device;
+typedef uint64_t gp_device_button;
 
-typedef void* (*PFN_gpAllocateMemory)(void* userData, size_t size);
-typedef void (*PFN_gpFreeMemory)(void* userData, void* ptr);
-
-
-typedef enum GpResult {
+typedef enum gp_result {
 	GP_SUCCESS,
 	GP_GENERIC_ERROR,
-} GpResult;
+} gp_result;
 
-typedef enum GpDeviceType {
+typedef enum gp_device_type {
 	GP_DEVICE_TYPE_KEYBOARD,
 	GP_DEVICE_TYPE_MOUSE,
 	GP_DEVICE_TYPE_GAMEPAD,
 	GP_DEVICE_TYPE_TOUCH,
-} GpDeviceType;
+} gp_device_type;
 
-typedef enum GpDeviceStatus {
+typedef enum gp_device_status {
 	GP_DEVICE_STATUS_UNAVAILABLE,
 	GP_DEVICE_STATUS_OK,
 	GP_DEVICE_STATUS_LOW_BATTERY,
-} GpDeviceStatus;
+} gp_device_status;
 
-typedef enum GpDeviceButtonType {
+typedef enum gp_device_button_type {
 	GP_DEVICE_BUTTON_TYPE_FLOAT,
 	GP_DEVICE_BUTTON_TYPE_FLOAT2,
 	GP_DEVICE_BUTTON_TYPE_FLOAT3,
 	GP_DEVICE_BUTTON_TYPE_BOOL,
-} GpDeviceButtonType;
+} gp_device_button_type;
 
-typedef struct GpFloat2 {
-	GpFloat x;
-	GpFloat y;
-} GpFloat2;
+typedef struct gp_float2 {
+	gp_float x;
+	gp_float y;
+} gp_float2;
 
-typedef struct GpFloat3 {
-	GpFloat x;
-	GpFloat y;
-	GpFloat z;
-} GpFloat3;
+typedef struct gp_float3 {
+	gp_float x;
+	gp_float y;
+	gp_float z;
+} gp_float3;
 
-typedef struct GpButtonState {
+typedef struct gp_button_state {
 	union {
-		GpFloat f;
-		GpFloat2 f2;
-		GpFloat3 f3;
-		GpBool b;
+		gp_float f;
+		gp_float2 f2;
+		gp_float3 f3;
+		gp_bool b;
 	} data;
-} GpButtonState;
+} gp_button_state;
 
-typedef struct GpDeviceInputState {
-	GpSize numButtons;
-	GpButtonState buttons[];
-} GpDeviceInputState;
+typedef struct gp_device_input_state {
+	gp_size num_buttons;
+	gp_button_state buttons[];
+} gp_device_input_state;
 
+typedef void* (*gp_PFN_allocate_memory)(void* user_data, size_t size);
+typedef void (*gp_PFN_free_memory)(void* user_data, void* ptr);
+typedef void (*gp_PFN_poll_input_device)(gp_device device, void* user_data, gp_device_input_state* input_state);
 
-typedef void (*PFN_gpPollInputDevice)(GpDevice device, void* userData, GpDeviceInputState* inputState);
+typedef struct gp_context_create_desc {
+	gp_PFN_allocate_memory allocate_memory;
+	gp_PFN_free_memory free_memory;
+	void* memory_user_data;
+} gp_context_create_desc;
 
-typedef struct GpContextCreateInfo {
-	PFN_gpAllocateMemory allocateMemory;
-	PFN_gpFreeMemory freeMemory;
-	void* memoryUserData;
-} GpContextCreateInfo;
+typedef struct gp_register_input_device_desc {
+	gp_device_type device_type;
+	gp_size device_number;
+	gp_PFN_poll_input_device poll_input_device;
+	void* user_data;
+} gp_register_input_device_desc;
 
-
-typedef struct GpRegisterInputDeviceInfo {
-	GpDeviceType deviceType;
-	GpSize deviceNumber;
-	PFN_gpPollInputDevice pollInputDevice;
-	void* userData;
-} GpRegisterInputDeviceInfo;
-
-typedef struct GpDeviceButtonInfo {
-	GpBool isValid;
-	GpDevice device;
-	GpDeviceButton button;
-	GpDeviceButtonType type;
-} GpDeviceButtonInfo;
+typedef struct gp_device_button_info {
+	gp_bool is_valid;
+	gp_device device;
+	gp_device_button button;
+	gp_device_button_type type;
+} gp_device_button_info;
 
 // Context/global
-GpResult gpCreateContext(const GpContextCreateInfo* pCreateInfo, GpContext* context);
-void gpDestroyContext(GpContext context);
-void gpSetContextDisplaySize(GpContext context, GpSize width, GpSize height);
-void gpProcessContext(GpContext context);
-GpResult gpGetAnyButtonDown(GpContext context, GpSize maxButtonNum, GpDeviceButtonInfo* deviceButtonInfos);
-GpResult gpGetDeviceByType(GpContext context, GpDeviceType deviceType, GpSize deviceNumber);
-// TODO device sync?
+gp_result gp_create_context(const gp_context_create_desc* create_desc, gp_context* context);
+void gp_destroy_context(gp_context context);
+void gp_set_context_display_size(gp_context context, gp_size width, gp_size height);
+void gp_process_context(gp_context context);
+gp_result gp_get_any_button_down(gp_context context, gp_size max_button_num, gp_device_button_info* device_button_infos);
+gp_result gp_get_device_by_type(gp_context context, gp_device_type device_type, gp_size device_number);
 
 // Device
-GpResult gpRegisterDevice(GpContext context, const GpRegisterInputDeviceInfo* pRegisterInfo, GpDevice* device);
-GpResult gpCreateAndRegisterPlatformDevice(GpContext context, GpDeviceType type, GpDevice* device);
-void gpUnregisterDevice(GpDevice device);
-GpResult gpGetDeviceStatus(GpDevice device, GpDeviceStatus* status);
-GpResult gpAllocateDeviceInputState(GpDevice device, GpDeviceInputState** inputState);
-void gpFreeDeviceInputState(GpDevice device, GpDeviceInputState* inputState);
-GpResult gpUpdateDeviceInputState(GpDevice device, GpDeviceInputState* inputState);
-GpResult gpGetDeviceButtonInfo(GpDevice device, GpDeviceButton deviceButton, GpDeviceButtonInfo* deviceButtonInfo);
-GpResult gpGetDeviceButtonName(GpDevice device, GpDeviceButton deviceButton, GpSize maxNameLen, char* name);
-GpResult gpGetDeviceButtonByName(GpDevice device, const char* name, GpDeviceButton* deviceButton);
-GpResult gpGetDeviceAnyButtonDown(GpDevice device, GpSize maxButtonNum, GpDeviceButtonInfo* deviceButtonInfos);
+gp_result gp_register_device(gp_context context, const gp_register_input_device_desc* register_desc, gp_device* device);
+gp_result gp_create_and_register_platform_device(gp_context context, gp_device_type type, gp_device* device);
+void gp_unregister_device(gp_device device);
+gp_result gp_get_device_status(gp_device device, gp_device_status* status);
+gp_result gp_allocate_device_input_state(gp_device device, gp_device_input_state** input_state);
+void gp_free_device_input_state(gp_device device, gp_device_input_state* input_state);
+gp_result gp_update_device_input_state(gp_device device, gp_device_input_state* input_state);
+gp_result gp_get_device_button_info(gp_device device, gp_device_button device_button, gp_device_button_info* device_button_info);
+gp_result gp_get_device_button_name(gp_device device, gp_device_button device_button, gp_size max_name_len, char* name);
+gp_result gp_get_device_button_by_name(gp_device device, const char* name, gp_device_button* device_button);
+gp_result gp_get_device_any_button_down(gp_device device, gp_size max_button_num, gp_device_button_info* device_button_infos);
 // TODO deadzones
 
 // State helpers
-GpBool gpIsDown(const GpDeviceInputState* inputState, GpDeviceButton deviceButton);
-GpFloat gpGetFloat(const GpDeviceInputState* inputState, GpDeviceButton deviceButton);
+gp_bool gp_is_down(const gp_device_input_state* input_state, gp_device_button device_button);
+gp_float gp_get_float(const gp_device_input_state* input_state, gp_device_button device_button);
 
 // TODO button change events/listeners
 
@@ -129,7 +130,7 @@ GpFloat gpGetFloat(const GpDeviceInputState* inputState, GpDeviceButton deviceBu
 
 // TODO input mapping
 
-// TODO input processor chaining/graph
+// TODO new: input processor chaining/graph
 
 // Cut feature (for now): network sync, gestures, thread-safe adding of button events, 
 // debug rendering, input recording/playback.
